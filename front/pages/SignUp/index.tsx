256482325304c152cb3c1abd1d85044aff02da15
San-Goon <tigerlove95@naver.com>
2022-10-31 10:04:33 +0900

    feat: Publishing Signup page

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Button, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
 
 const SignUp = () => {
-  return <div>회원가입</div>;
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+
+  const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }, []);
+  const onChangeNickname = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(e.target.value);
+  }, []);
+  const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, []);
+  const onChangePasswordCheck = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordCheck(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log('submit!', email, nickname, password, passwordCheck);
+    },
+    [email, nickname, password, passwordCheck],
+  );
+
+  return (
+    <div id="container">
+      <Header>Slack</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="nickname" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <a href="/login">로그인 하러가기</a>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default SignUp;
