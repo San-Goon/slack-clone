b32181fc516d5a86fae27dad91371bc8f902ea41
San-Goon <tigerlove95@naver.com>
2022-11-01 11:24:00 +0900

    chore: add DM page

--- front/layouts/App/index.tsx
+++ front/layouts/App/index.tsx
@@ -5,6 +5,7 @@ import loadable from '@loadable/component';
 const LogIn = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
 const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/login" element={<LogIn />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/workspace/channel" element={<Channel />} />
+      <Route path="/workspace/dm" element={<DirectMessage />} />
     </Routes>
   );
 };
