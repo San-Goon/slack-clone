05f35b953052c5124e68a1b0c9cdfe97d9d24ea2
San-Goon <tigerlove95@naver.com>
2022-11-01 11:00:47 +0900

    chore: add profile img by using gravatar

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -3,6 +3,8 @@ import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Header, ProfileImg, RightMenu } from '@layouts/Workspace/styles';
+import gravatar from 'gravatar';
 
 const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
   const { data, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -26,6 +28,13 @@ const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onClickLogout}>로그아웃</button>
       {children}
     </div>
