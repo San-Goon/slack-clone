fd77bbc4ded827bd01d11f22c193bc3686fe53e7
San-Goon <tigerlove95@naver.com>
2022-11-02 22:49:48 +0900

    refactor: downgrade SWR to 2 for connect websocket with back

--- front/hooks/useSocket.ts
+++ front/hooks/useSocket.ts
@@ -1,10 +1,10 @@
-import { io, Socket } from 'socket.io-client';
+import io from 'socket.io-client';
 import { useCallback } from 'react';
 
-const backUrl = 'http://localhost:3095';
+const backUrl = 'https://sleact.nodebird.com';
 
-const sockets: { [key: string]: Socket } = {};
-const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
+const sockets: { [key: string]: SocketIOClient.Socket } = {};
+const useSocket = (workspace?: string): [SocketIOClient.Socket | undefined, () => void] => {
   // 연결을 끊는 함수. 끊지않으면 워크스페이스가 변경되어도 계속 메세지를 받게됨.
   const disconnect = useCallback(() => {
     if (workspace) {
@@ -17,7 +17,7 @@ const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
 
   // socket.io 를 쓸수 있게하는 함수. 재연결 방지를 위해 이프문에 삽입
   if (!sockets[workspace]) {
-    sockets[workspace] = io(`${backUrl}/ws-${workspace}`, {
+    sockets[workspace] = io.connect(`${backUrl}/ws-${workspace}`, {
       transports: ['websocket'],
     });
   }
