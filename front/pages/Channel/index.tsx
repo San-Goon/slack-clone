bdf766329a08e3f5efbfaa79b78166099207d733
San-Goon <tigerlove95@naver.com>
2022-11-02 11:36:27 +0900

    feat: build DirectMessage Page

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Container, Header } from '@pages/Channel/styles';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
 
 const Channel = () => {
-  return <div>로그인완료</div>;
+  const [chat, onChangeChat, setChat] = useInput();
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setChat('');
+    },
+    [chat],
+  );
+
+  return (
+    <Container>
+      <Header>채널!</Header>
+      <ChatList />
+      <ChatBox chat={chat} onSubmit={onSubmit} onChangeChat={onChangeChat} />
+    </Container>
+  );
 };
 
 export default Channel;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import { useParams } from 'react-router';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
-  return <div>DirectMessage</div>;
+  const { workspace, id } = useParams();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+
+  const [chat, onChangeChat, setChat] = useInput();
+
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setChat('');
+    },
+    [chat],
+  );
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData?.email, { s: '24px', d: 'retro' })} alt={userData?.nickname} />
+      </Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmit={onSubmit} />
+    </Container>
+  );
 };
 
 export default DirectMessage;
